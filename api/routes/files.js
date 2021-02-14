555e17b690933fb9a26cbecd805c38dfcb792490
Fares <fares.osman@outlook.com>
2021-02-13 21:49:00 -0500

    Added comments for file post route handler

--- api/routes/files.js
+++ api/routes/files.js
@@ -11,3 +11,8 @@ router.post(
 );
 
 module.exports = router;
+
+// When user is trying to upload a file here are the steps 
+// 1) The first middle ware that is ran will be passport, which will authenitcate the request based on JWT from the cookie header 
+// 2) The second middle ware will be ran, which is uploading the file that was sleected by the user to S3 
+// 3) The createFile controller action will be invoked
