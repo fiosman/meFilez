b243befaaeecb56004a1aedce12ae4fbf24436b6
Fares <fares.osman@outlook.com>
2021-02-26 21:34:08 -0500

    Update file schema; add ancestors field

--- models/File.js
+++ models/File.js
@@ -31,16 +31,5 @@ const FileSchema = new Schema(
   { timestamps: true }
 );
 
-FileSchema.post("save", function (file, next) {
-  File.findById(this.parentId)
-    .exec()
-    .then((file) => {
-      console.log(file.ancestors);
-      this.ancestors.push(...file.ancestors, file._id);
-    })
-    .catch((err) => console.log(err));
-  next();
-});
-
 const File = mongoose.model("file", FileSchema);
 module.exports = File;
