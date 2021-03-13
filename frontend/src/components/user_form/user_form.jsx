b877e0fbcba0697a2e51b42f2f57a175ab6fc169
Fares <fares.osman@outlook.com>
2021-03-12 22:57:19 -0500

    Template for user form; instsall react bootstrap

---
+++ frontend/src/components/user_form/user_form.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+
+class UserForm extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return <div></div>;
+  }
+}
+
+export default UserForm;
