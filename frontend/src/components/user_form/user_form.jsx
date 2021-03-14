0324474183ed551bb55df0ca82a09f77e6290030
Fares <fares.osman@outlook.com>
2021-03-13 23:56:14 -0500

    Start writing functional component for user form

--- frontend/src/components/user_form/user_form.jsx
+++ frontend/src/components/user_form/user_form.jsx
@@ -1,15 +1,38 @@
 import React from "react";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
+import { useState } from "react";
 
-class UserForm extends React.Component {
-  constructor(props) {
-    super(props);
-  }
+function UserForm() {
+  const [userInfo, setUserInfo] = useState({
+    username: "",
+    email: "",
+    password: "",
+    password1: "",
+  });
 
-  render() {
-    return <div></div>;
-  }
+  function handleChange(e) {}
+
+  function handleSubmit(e) {}
+
+  return (
+    <Form>
+      <Form.Group>
+        <Form.Label>Email Address</Form.Label>
+        <Form.Control type="email" placeholder="Enter email" />
+      </Form.Group>
+      <Form.Group>
+        <Form.Label>Username</Form.Label>
+        <Form.Control type="text" placeholder="Username" />
+      </Form.Group>
+      <Form.Group>
+        <Form.Label>Please enter your password</Form.Label>
+        <Form.Control type="password" placeholder="Password" />
+        <Form.Label>Please enter your password again</Form.Label>
+        <Form.Control type="password" placeholder="Password" />
+      </Form.Group>
+    </Form>
+  );
 }
 
 export default UserForm;
