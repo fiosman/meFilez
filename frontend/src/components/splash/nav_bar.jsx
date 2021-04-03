2bf906a8518ac16382d762a0843b60a591d70f11
Fares <fares.osman@outlook.com>
2021-04-03 00:41:52 -0400

    Update navbar styling

--- frontend/src/components/splash/nav_bar.jsx
+++ frontend/src/components/splash/nav_bar.jsx
@@ -1,10 +1,22 @@
 import React from "react";
+import Navbar from "react-bootstrap/Navbar";
 import Image from "react-bootstrap/Image";
-import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row";
 
-function NavBar() {
-  return <Image src="/logo.png" />;
+function MainHeader() {
+  return (
+    <Navbar bg="custom" variant="dark" className="nav-bar">
+      <Navbar.Brand>
+        <Image src="/logo.png" width="140px" height="110px" /> Access your files
+        anywhere, anytime.
+      </Navbar.Brand>
+      <Navbar.Toggle />
+      <Navbar.Collapse className="justify-content-end">
+        <Navbar.Text>
+          Signed in as: <a href="#login">Mark Otto</a>
+        </Navbar.Text>
+      </Navbar.Collapse>
+    </Navbar>
+  );
 }
 
-export default NavBar;
+export default MainHeader;
