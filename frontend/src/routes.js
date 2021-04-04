dfec695ddd65f736333ac9bd637086fec5ecbbe3
Fares <fares.osman@outlook.com>
2021-04-03 23:31:51 -0400

    Create protected and authroutes

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -2,13 +2,14 @@ import React from "react";
 import Splash from "./components/splash/splash";
 import FileList from "./components/files/file_list";
 import { Route, Switch } from "react-router-dom";
+import { ProtectedRoute, AuthRoute } from "./util/route_utils";
 
 export const Routes = () => {
   return (
     <div>
       <Switch>
-        <Route exact path="/" component={Splash} />
-        <Route exact path="/files" component={FileList} />
+        <AuthRoute exact path="/" component={Splash} />
+        <ProtectedRoute exact path="/files" component={FileList} />
       </Switch>
     </div>
   );
