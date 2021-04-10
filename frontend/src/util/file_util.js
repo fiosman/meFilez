d86768dad83f30980618c1e28b8adfda71574d3b
Fares <fares.osman@outlook.com>
2021-04-10 11:54:34 -0400

    Write getFile utility function

--- frontend/src/util/file_util.js
+++ frontend/src/util/file_util.js
@@ -3,3 +3,7 @@ import axios from "axios";
 export const getAllFiles = () => {
   return axios.get("api/files/");
 };
+
+export const getFile = (folderId) => {
+  return axios.get(`api/files/${folderId}`);
+};
