a7809676162a63f05529dcce514b4f649aedbffb
Fares <fares.osman@outlook.com>
2021-04-11 19:26:16 -0400

    Write action creators for search

--- frontend/src/actions/file_actions.js
+++ frontend/src/actions/file_actions.js
@@ -1,9 +1,20 @@
 import { getAllFiles, getFolder } from "../util/file_util";
 export const RECEIVE_FILES = "RECEIVE_FILES";
+export const RECEIVE_SEARCHED_FILES = "RECEIVE_SEARCHED_FILES";
+export const CLEAR_FILTERS = "CLEAR_FILTERS";
 
 export const receiveFiles = (files) => ({
   type: RECEIVE_FILES,
   files: files.data,
+});
+
+export const receiveSearchedFiles = (searchTerm) => ({
+  type: RECEIVE_SEARCHED_FILES,
+  searchTerm,
+});
+
+export const clearFilters = () => ({
+  type: CLEAR_FILTERS,
 });
 
 export const fetchFiles = () => (dispatch) =>
