b43308bdb39ba88d3ad22971c90d70919264543d
Fares <fares.osman@outlook.com>
2021-04-11 19:40:10 -0400

    Update entitiesReducer to include filtersReducer

--- frontend/src/reducers/entities_reducer.js
+++ frontend/src/reducers/entities_reducer.js
@@ -1,11 +1,12 @@
-// import filesReducer from "./files_reducer";
 import userReducer from "./user_reducer";
 import filesReducer from "./files_reducer";
+import filtersReducer from "./filters_reducer";
 import { combineReducers } from "redux";
 
 const entitiesReducer = combineReducers({
   files: filesReducer,
   user: userReducer,
+  filters: filtersReducer,
 });
 
 export default entitiesReducer;
