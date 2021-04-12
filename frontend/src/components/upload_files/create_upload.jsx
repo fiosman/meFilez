771f5b5c9b948bfc7c8d671e8d0498afa092c4ea
Fares <fares.osman@outlook.com>
2021-04-11 22:47:03 -0400

    Fix previous page rendering

--- frontend/src/components/upload_files/create_upload.jsx
+++ frontend/src/components/upload_files/create_upload.jsx
@@ -1,10 +1,25 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faFolderPlus, faFileUpload } from "@fortawesome/free-solid-svg-icons";
+import {
+  faFolderPlus,
+  faFileUpload,
+  faArrowLeft,
+} from "@fortawesome/free-solid-svg-icons";
+import { withRouter } from "react-router";
 
-function CreateUpload() {
+function CreateUpload(props) {
+  function goToPreviousPage() {
+    return props.history.goBack();
+  }
   return (
     <div className="folder-upload-add">
+      {props.match.url === "/files" ? (
+        ""
+      ) : (
+        <span className="previous-page">
+          <FontAwesomeIcon icon={faArrowLeft} onClick={goToPreviousPage} />
+        </span>
+      )}
       <span className="add-folder">
         <FontAwesomeIcon icon={faFolderPlus} />
       </span>
@@ -15,4 +30,4 @@ function CreateUpload() {
   );
 }
 
-export default CreateUpload;
+export default withRouter(CreateUpload);
