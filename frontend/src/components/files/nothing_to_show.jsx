371b03d29dbc741aaf97252ff977357deec62f0d
Fares <fares.osman@outlook.com>
2021-04-12 21:23:49 -0400

    reset search bar input after clearing filters

--- frontend/src/components/files/nothing_to_show.jsx
+++ frontend/src/components/files/nothing_to_show.jsx
@@ -1,27 +1,11 @@
 import React from "react";
-import Button from "react-bootstrap/Button";
-import { withRouter } from "react-router";
-import { useSelector, useDispatch } from "react-redux";
-import { clearFilters } from "../../actions/file_actions";
 
-function NothingToShow(props) {
-  const { search } = useSelector((state) => state.filters);
-  const dispatch = useDispatch();
-
+function NothingToShow() {
   return (
     <div className="nothing-to-show">
       <h3>Nothing to show here...</h3>
-      {search !== "" ? (
-        <Button variant="custom" onClick={() => dispatch(clearFilters())}>
-          Clear Filters
-        </Button>
-      ) : (
-        <Button onClick={() => props.history.goBack()} variant="custom">
-          Go Back
-        </Button>
-      )}
     </div>
   );
 }
 
-export default withRouter(NothingToShow);
+export default NothingToShow;
--- frontend/src/components/search_files/search.jsx
+++ frontend/src/components/search_files/search.jsx
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from "react";
 import FormControl from "react-bootstrap/FormControl";
 import { useDispatch, useSelector } from "react-redux";
 import { receiveSearchTerm } from "../../actions/file_actions";
+import Button from "react-bootstrap/Button";
+import { clearFilters } from "../../actions/file_actions";
 
 function Search() {
   const [searchTerm, setSearchTerm] = useState(
@@ -11,7 +13,12 @@ function Search() {
   const dispatch = useDispatch();
 
   function handleChange(e) {
-    return setSearchTerm(e.target.value);
+    setSearchTerm(e.target.value);
+  }
+
+  function handleClearFilters(e) {
+    dispatch(clearFilters());
+    setSearchTerm("");
   }
 
   useEffect(() => {
@@ -19,13 +26,21 @@ function Search() {
   }, [searchTerm, dispatch]);
 
   return (
-    <div className="search-bar">
+    <div className="search-container">
       <FormControl
+        className="search-box"
         type="text"
         placeholder="Search..."
         onChange={handleChange}
         value={searchTerm}
       />
+      <Button
+        className="clear-filters"
+        variant="custom"
+        onClick={handleClearFilters}
+      >
+        Clear Filters
+      </Button>
     </div>
   );
 }
