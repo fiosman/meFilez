dbef8db026e5fe0a114f5ea2208725e21ba4dba1
Fares <fares.osman@outlook.com>
2021-04-13 21:51:59 -0400

    Update fileReducer with REMOVE_FILE

--- frontend/src/reducers/files_reducer.js
+++ frontend/src/reducers/files_reducer.js
@@ -1,4 +1,4 @@
-import { RECEIVE_FILES } from "../actions/file_actions";
+import { RECEIVE_FILES, REMOVE_FILE } from "../actions/file_actions";
 import { LOGOUT_CURRENT_USER } from "../actions/user_actions";
 
 const filesReducer = (state = {}, action) => {
@@ -7,6 +7,10 @@ const filesReducer = (state = {}, action) => {
   switch (action.type) {
     case RECEIVE_FILES:
       return Object.assign({}, action.files);
+    case REMOVE_FILE:
+      let currentState = Object.assign({}, state);
+      delete currentState[action.fileId];
+      return currentState;
     case LOGOUT_CURRENT_USER:
       return {};
     default:
