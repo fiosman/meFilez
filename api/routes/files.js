e518c929dfb9ce4446e4479881ebb271dc06b2b8
Fares <fares.osman@outlook.com>
2021-04-24 17:18:36 -0400

    Update userAuth on backend

--- api/routes/files.js
+++ api/routes/files.js
@@ -10,21 +10,7 @@ const {
 } = require("../controllers/fileController");
 const upload = require("../../services/fileUpload");
 const deleteBucketFile = require("../../services/fileDelete");
-const File = require("../../models/File");
-
-const userAuthorization = (req, res, next) => {
-  const currentUser = req.user._id;
-  const fileId = req.params.fileId;
-  File.findById(fileId)
-    .then((file) => {
-      return file.owner.equals(currentUser)
-        ? next()
-        : res
-            .status(400)
-            .json(["You are not authorized to complete this action."]);
-    })
-    .catch((err) => res.status(404).json(["File was not found"]));
-};
+const userAuthorization = require("../../services/userAuth");
 
 router.post(
   "/",
---
+++ services/userAuth.js
@@ -0,0 +1,18 @@
+const File = require("../models/File");
+
+const userAuthorization = (req, res, next) => {
+  console.log("IT WORKING");
+  const currentUser = req.user._id;
+  const fileId = req.params.fileId;
+  File.findById(fileId)
+    .then((file) => {
+      return file.owner.equals(currentUser)
+        ? next()
+        : res
+            .status(400)
+            .json(["You are not authorized to complete this action."]);
+    })
+    .catch((err) => res.status(404).json(["File was not found"]));
+};
+
+module.exports = userAuthorization;
